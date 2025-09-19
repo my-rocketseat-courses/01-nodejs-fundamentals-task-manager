4289ebbc0b29dc4dcf1ee374580f5a236921224e
Laize Ferraz <laizeferraz@gmail.com>
2025-09-19 20:21:43 +1000

    delete task

--- src/routes.js
+++ src/routes.js
@@ -31,7 +31,11 @@ export const routes = [
     method: "DELETE",
     path: buildRoutePath("/tasks/:id"),
     handler: (req, res) => {
-      return res.end()
+      const { id } = req.params
+
+      database.delete("tasks", id)
+      
+      return res.writeHead(204).end()
     }
   },
 ]
